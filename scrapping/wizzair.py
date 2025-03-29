0541e1c3c5321b066f044a4fdc611ca7b28d9456
Dominik Ficek <dominik.ficek@email.cz>
2025-03-29 14:40:38 +0100

    wizzair api version update during run

--- scrapping/wizzair.py
+++ scrapping/wizzair.py
@@ -9,9 +9,14 @@ from .utils import InvalidFLightException
 class WizzAirScrapper:
     def __init__(self, adults: int=2) -> None:
         self.adults = adults
+        
+        self.api_version = self.get_api_version()
+
+    def get_api_version(self) -> str:
         session = requests.Session(impersonate='chrome', proxy=os.getenv('stickyproxy'))
         resp = session.get('https://www.wizzair.com/buildnumber')
-        self.api_version = resp.text.split('/')[-1]
+        api_version = resp.text.split('/')[-1]
+        return api_version
 
     def get_price(self, departure_station: str, arrival_station: str, date: str) -> float | None:
         data = {
@@ -27,8 +32,18 @@ class WizzAirScrapper:
         }
 
         session = requests.Session(impersonate='chrome', proxy=os.getenv('stickyproxy'))
-        resp = session.post(f'https://be.wizzair.com/{self.api_version}/Api/asset/farechart', data=json.dumps(data), headers={'Content-type': 'application/json; charset=UTF-8'})
-        resp.raise_for_status()
+        try:
+            resp = session.post(f'https://be.wizzair.com/{self.api_version}/Api/asset/farechart', data=json.dumps(data), headers={'Content-type': 'application/json; charset=UTF-8'})
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            new_api_version = self.get_api_version()
+            if new_api_version != self.api_version:
+                self.api_version = new_api_version
+                resp = session.post(f'https://be.wizzair.com/{self.api_version}/Api/asset/farechart', data=json.dumps(data), headers={'Content-type': 'application/json; charset=UTF-8'})
+                resp.raise_for_status()
+            else:
+                raise e
+            
         resp_data = resp.json()
 
         target_date = date + 'T00:00:00'
